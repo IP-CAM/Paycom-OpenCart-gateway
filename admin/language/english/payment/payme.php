a69e7f7a0b1ece63cf19fefbe56a37d6618d09ac
xakbar <akbar.khayitov@gmail.com>
2018-02-21 10:23:03 +0500

    Create payme.php

---
+++ admin/language/english/payment/payme.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']      = 'Payme';
+
+// Text 
+$_['text_payment']       = 'Payment';
+$_['text_success']       = 'Success: You have modified Payme account details!';
+$_['text_payme']       = '<a href="https://www.payme.uz" taget="_blank"><img src="view/image/payment/payme-logo.png" alt="Payme" title="Payme"  /></a>';
+      
+// Entry
+$_['entry_test']     = 'Sandbox Mode:';
+$_['entry_merchant']     = 'Merchant ID:';
+$_['entry_security']     = 'Security Code:';
+$_['entry_callback']     = 'Alert URL:<br /><span class="help">This has to be set in the Payme control panel.</span>';
+$_['entry_total']        = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_order_status'] = 'Order Status:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Status:';
+$_['entry_sort_order']   = 'Sort Order:';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Payme!';
+$_['error_merchant']     = 'Merchant ID Required!';
+$_['error_security']     = 'Security Code Required!';
+?>
