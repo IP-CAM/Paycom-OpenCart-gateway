d9eefcd91aa2c8508c40bb885ffc7a7e6d93f9ba
xakbar <akbar.khayitov@gmail.com>
2018-02-21 10:24:22 +0500

    Create payme.php

---
+++ admin/language/russian/payme.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']      = 'Payme';
+
+// Text 
+$_['text_payment']       = 'Оплата';
+$_['text_success']       = 'Успешно: вы изменили данные учетной записи Payme!';
+$_['text_payme']       = '<a href="https://www.payme.uz" taget="_blank"><img src="view/image/payment/payme-logo.png" alt="Payme" title="Payme"  /></a>';
+      
+// Entry
+$_['entry_test']     = 'Песочница Paycom:';
+$_['entry_merchant']     = 'Merchant ID:';
+$_['entry_security']     = 'Security Code:';
+$_['entry_callback']     = 'Alert URL:<br /><span class="help">Это должно быть установлено на панели управления Payme.</span>';
+$_['entry_total']        = 'Total:<br /><span class="help">Выписка всего заказ должен достичь, прежде чем этот метод оплаты становится активным.</span>';
+$_['entry_order_status'] = 'Статус заказа:';
+$_['entry_geo_zone']     = 'Geo Zone:';
+$_['entry_status']       = 'Статус:';
+$_['entry_sort_order']   = 'Порядок сортировки:';
+
+// Error
+$_['error_permission']   = 'Предупреждение: у вас нет разрешения на изменение платежа Payme!';
+$_['error_merchant']     = 'Merchant ID обязательно!';
+$_['error_security']     = 'Security Code обязательно!';
+?>
